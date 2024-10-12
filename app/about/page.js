f2fc36372a4f4c233ecfab2cf940faa2feb46c4f
amir nabiizade <amirnabiizade@gmail.com>
2024-10-12 23:14:36 +0330

    config

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,8 @@
+
+export const metadata ={
+  title : "About"
+}
+
 function About() {
   return <h1>About the wild oasis</h1>
 }
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,8 @@
+
+export const metadata = {
+  title : "Cabins"
+}
+
 function Cabins() {
   return (
     <div>
