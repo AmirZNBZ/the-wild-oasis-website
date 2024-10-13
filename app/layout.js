78d84978bdf8d743e6b106d0af9033adfebae55c
amirnabiizade <amirnabiizade@gmail.com>
2024-10-13 18:59:57 +0330

    added new routes and page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import "@/_styles/globals.css";
-import Header from "./_components/Header";
+import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
 import { Josefin_Sans } from "next/font/google";
 
 const josefin = Josefin_Sans({
@@ -25,8 +25,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
